3783fc58abeb3bf7a277b685ce7478c619578702
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2020-12-07 15:45:33 +0900

    userモデルバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname , presence: true
-  validates :last_name , presence: true
-  validates :first_name , presence: true
-  validates :last_name_kana , presence: true ,format: { with: /\p{katakana}/}
-  validates :first_name_kana , presence: true ,format: { with: /\p{katakana}/}
+  validates :last_name , presence: true,format: { with: /\A[^ -~｡-ﾟ]+\z/,message: "Full-width characters"}
+  validates :first_name , presence: true,format: { with: /\A[^ -~｡-ﾟ]+\z/,message: "Full-width characters"}
+  validates :last_name_kana , presence: true ,format: { with: /\p{katakana}/,message: "Full-width katakana characters"}
+  validates :first_name_kana , presence: true ,format: { with: /\p{katakana}/,message: "Full-width katakana characters"}
   validates :birthday , presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
 
 end
