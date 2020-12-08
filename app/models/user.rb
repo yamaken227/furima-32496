1ae1a113b4dc30088a2b9c5f3ac77a6483fc32c2
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2020-12-08 11:34:35 +0900

    ユーザー管理機能コード整形

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname , presence: true
-  validates :last_name , presence: true,format: { with: /\A[^ -~｡-ﾟ]+\z/,message: "Full-width characters"}
-  validates :first_name , presence: true,format: { with: /\A[^ -~｡-ﾟ]+\z/,message: "Full-width characters"}
-  validates :last_name_kana , presence: true ,format: { with: /\p{katakana}/,message: "Full-width katakana characters"}
-  validates :first_name_kana , presence: true ,format: { with: /\p{katakana}/,message: "Full-width katakana characters"}
-  validates :birthday , presence: true
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
-
+  validates :nickname, presence: true
+  validates :last_name, presence: true, format: { with: /\A[^ -~｡-ﾟ]+\z/, message: 'Full-width characters' }
+  validates :first_name, presence: true, format: { with: /\A[^ -~｡-ﾟ]+\z/, message: 'Full-width characters' }
+  validates :last_name_kana, presence: true, format: { with: /\p{katakana}/, message: 'Full-width katakana characters' }
+  validates :first_name_kana, presence: true, format: { with: /\p{katakana}/, message: 'Full-width katakana characters' }
+  validates :birthday, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 end
