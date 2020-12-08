8a97a515075ff0afe363dc15e79f4cce0b504d18
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2020-12-08 19:05:18 +0900

    商品出品機能バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,14 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :days_to_ship
   has_one_attached :image
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :price, presence: true
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :fee_burden_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :days_to_ship_id, numericality: { other_than: 1 }
 end
