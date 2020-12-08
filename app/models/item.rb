1921c07c06b70eabd0cd88b64c05313d2ee4140b
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2020-12-08 18:05:01 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :fee_burden
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :days_to_ship
   has_one_attached :image
 end
