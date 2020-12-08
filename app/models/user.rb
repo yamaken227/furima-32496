edbbff2bce129365a82301cbefa1ed98b4509908
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2020-12-08 12:26:11 +0900

    userモデルのバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[^ -~｡-ﾟ]+\z/, message: 'Full-width characters' }
-  validates :first_name, presence: true, format: { with: /\A[^ -~｡-ﾟ]+\z/, message: 'Full-width characters' }
-  validates :last_name_kana, presence: true, format: { with: /\p{katakana}/, message: 'Full-width katakana characters' }
-  validates :first_name_kana, presence: true, format: { with: /\p{katakana}/, message: 'Full-width katakana characters' }
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: { with: /\A[^ -~｡-ﾟ]+\z/, message: 'Full-width characters' }
+    validates :first_name, format: { with: /\A[^ -~｡-ﾟ]+\z/, message: 'Full-width characters' }
+    validates :last_name_kana, format: { with: /\p{katakana}/, message: 'Full-width katakana characters' }
+    validates :first_name_kana, format: { with: /\p{katakana}/, message: 'Full-width katakana characters' }
+    validates :birthday
+  end
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 end
